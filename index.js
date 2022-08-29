eb07ac6cedbcb84adc365ed065d6d2e33f753053
chris <chrishethertonweb@gmail.com>
2022-08-29 19:57:53 +0200

    added add and remove item

--- index.js
+++ index.js
@@ -2,5 +2,32 @@ const toDosStore = ["Shopping", "Home work", "Go to the gym"];
 
 // Write your code here
 
+class ToDos {
+
+  constructor(item) {
+
+    this.item = item
+    this.toDo = []
+
+  }
+
+  addItem(item) {
+    this.toDo.push(item)
+
+  }
+
+  removeItem(item) {
+
+    this.toDo = this.toDo.filter(this.ToDos.includes(item))
+
+  }
+}
+
+
+
+
+
+
+
 
 console.log(RenderToDosListTemplate())
